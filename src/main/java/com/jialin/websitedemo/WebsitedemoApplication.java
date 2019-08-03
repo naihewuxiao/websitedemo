8a4b1b2cc6c5ccca07164460385c0e38775b624c
181199610@qq.com <181199610@qq.com>
2019-08-03 10:01:07 +0800

    初试Spring boot 开发

---
+++ src/main/java/com/jialin/websitedemo/WebsitedemoApplication.java
@@ -0,0 +1,13 @@
+package com.jialin.websitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsitedemoApplication.class, args);
+	}
+
+}

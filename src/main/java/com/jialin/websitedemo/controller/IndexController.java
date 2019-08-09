46d4b619ed7ce09ba4ee8fc7b642757bb7533791
181199610@qq.com <181199610@qq.com>
2019-08-09 11:50:21 +0800

    初试Spring boot 开发

--- src/main/java/com/jialin/websitedemo/controller/IndexController.java
+++ src/main/java/com/jialin/websitedemo/controller/IndexController.java
@@ -1,23 +1,33 @@
 package com.jialin.websitedemo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class IndexController {
-    @RequestMapping(path = {"/","/index"})
-    @ResponseBody
+    @RequestMapping(path = {"/","/index"})  //接受浏览器对"localhost:8080/"或者"localhost:8080/index" 的请求
+    @ResponseBody   //对接受到的请求进行反应，直接返回body 没有头部等其他标签
     public String index(){
         return "hello susu";
     }
-    @RequestMapping(path = {"/profile/{userName}/{userId}"})
+
+
+    @RequestMapping(path = {"/profile/{userName}/{userId}"})  //{}中的变量表示将要传进来的//中的字符串
     @ResponseBody
     public String profile(@PathVariable("userId") int userId,
                           @PathVariable("userName") String userName,
                           @RequestParam(value = "type",defaultValue = "-10") int type){
         return String.format("hello %s,welcome go back ID:%d type:%d", userName,userId,type);
+    }
+
+    @RequestMapping(path = {"/test/{happy}"})
+    @ResponseBody
+    public String happy(@PathVariable("happy") String s){
+        return String.format("hello %s", s);
+    }
+
+    @RequestMapping(path = {"/vm"})
+    public String template(){
+        return "home";
     }
 }
